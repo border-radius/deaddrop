6e860e2a9509018c4eae4f54aa446c944c25bb4b
border-radius <pharshmaster@gmail.com>
2016-10-06 20:32:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict'
+
+var TelegramBot = require('node-telegram-bot-api')
+var express = require('express')
+var request = require('request')
+var token = require('./lib/token')
+var model = require('./lib/item')
+var config = require('./config.json')
+
+var bot = new TelegramBot(config.token, config.options)
+
+bot.on('message', msg => {
+  if (msg.text === '/start') {
+    return bot.sendMessage(msg.from.id, 'Send me any file or picture and I will host it online!')
+  }
+
+  var file = msg.document
+
+  if (!file && msg.photo) {
+    file = msg.photo[msg.photo.length - 1]
+  }
+
+  if (!file) {
+    return bot.sendMessage(msg.from.id, 'Just send me anything, dude.')
+  }
+
+  var item = new model({
+    id: token(),
+    author: [msg.from.first_name, msg.from.last_name].join(' '),
+    message: msg,
+    file_id: file.file_id,
+    file_type: file.file_type || 'jpg',
+    date: msg.date
+  })
+
+  item.save().then((item) => {
+    bot.sendMessage(msg.from.id, [
+      config.url,
+      [
+        item.id,
+        item.file_type
+      ].join('.')
+    ].join('/'))
+  })
+})
+
+var app = express()
+
+app.use(express.static(require('path').join(__dirname, 'static')))
+
+app.get('/:id.:type', (req, res, next) => {
+  model.findOne({
+    id: req.params.id
+  }).then(item => {
+    bot.getFileLink(item.file_id).then(link => request(link).pipe(res)).catch(next)
+  }).catch(next)
+})
+
+app.listen(3000)
+console.log('Server launched at', new Date())
